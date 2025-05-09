65a4d1980c9972478b3c2aa83e58efc21531c4cf
Sean Missingham <sean@missingham.com>
2025-05-09 08:47:24 -0500

    Podman autostart service causing issues locking socket

--- modules/nixos/virt/podman.nix
+++ modules/nixos/virt/podman.nix
@@ -80,32 +80,32 @@ in
 
     # Automatically start containers on boot
     # Use a systemd timer to periodically ensure containers are running
-    systemd.services.podman-autostart = {
-      enable = true;
-      description = "Automatically start containers with --restart=always tag";
-      path = [ pkgs.podman ];
-
-      serviceConfig = {
-        Type = "oneshot";
-        User = mainUser.username;
-        ExecStart = ''${pkgs.podman}/bin/podman start --all --filter restart-policy=always'';
-        # Restart on failure
-        Restart = "on-failure";
-        RestartSec = "30s";
-      };
-    };
-
-    # Add a timer to run the service periodically
-    systemd.timers.podman-autostart = {
-      enable = true;
-      description = "Timer for podman-autostart service";
-      wantedBy = [ "timers.target" ];
-
-      timerConfig = {
-        OnBootSec = "2min"; # Run 2 minutes after boot
-        OnUnitActiveSec = "10min"; # Run every 10 minutes thereafter
-        Unit = "podman-autostart.service";
-      };
-    };
+    # systemd.services.podman-autostart = {
+    #   enable = true;
+    #   description = "Automatically start containers with --restart=always tag";
+    #   path = [ pkgs.podman ];
+    #
+    #   serviceConfig = {
+    #     Type = "oneshot";
+    #     User = mainUser.username;
+    #     ExecStart = ''${pkgs.podman}/bin/podman start --all --filter restart-policy=always'';
+    #     # Restart on failure
+    #     Restart = "on-failure";
+    #     RestartSec = "30s";
+    #   };
+    # };
+    #
+    # # Add a timer to run the service periodically
+    # systemd.timers.podman-autostart = {
+    #   enable = true;
+    #   description = "Timer for podman-autostart service";
+    #   wantedBy = [ "timers.target" ];
+    #
+    #   timerConfig = {
+    #     OnBootSec = "2min"; # Run 2 minutes after boot
+    #     OnUnitActiveSec = "10min"; # Run every 10 minutes thereafter
+    #     Unit = "podman-autostart.service";
+    #   };
+    # };
   };
 }
