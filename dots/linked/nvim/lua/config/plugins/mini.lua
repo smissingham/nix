c75d3c3e202de0c3a90eac4123feb78a0ea827ac
Sean Missingham <sean@missingham.com>
2025-06-14 09:18:37 -0500

    Update mini surround keymap to not conflict with flash

--- dots/linked/nvim/lua/config/plugins/mini.lua
+++ dots/linked/nvim/lua/config/plugins/mini.lua
@@ -1,5 +1,21 @@
 return {
   { 'echasnovski/mini.bracketed', version = '*', config = function() require("mini.bracketed").setup() end },
   { 'echasnovski/mini.pairs',     version = '*', config = function() require("mini.pairs").setup() end },
-  { 'echasnovski/mini.surround',  version = '*', config = function() require("mini.surround").setup() end },
+  {
+    'echasnovski/mini.surround',
+    version = '*',
+    config = function()
+      require("mini.surround").setup({
+        mappings = {
+          add = "gsa",              -- Add surrounding in Normal and Visual modes
+          delete = "gsd",           -- Delete surrounding
+          find = "gsf",             -- Find surrounding (to the right)
+          find_left = "gsF",        -- Find surrounding (to the left)
+          highlight = "gsh",        -- Highlight surrounding
+          replace = "gsr",          -- Replace surrounding
+          update_n_lines = "gsn",   -- Update `n_lines`
+        },
+      })
+    end
+  },
 }
