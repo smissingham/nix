4b2da9d81885dd48e37a49a6890938d09165d782
Sean Missingham <sean@missingham.com>
2025-10-06 14:01:25 -0500

    Package Filen Desktop as Electron Built Distributable instead of Wrapped Binary

--- flakes/overlays/filen-desktop/package.nix
+++ flakes/overlays/filen-desktop/package.nix
@@ -5,12 +5,17 @@
   buildNpmPackage,
   fetchFromGitHub,
   makeDesktopItem,
-  desktopToDarwinBundle,
+  copyDesktopItems,
+  imagemagick,
+  makeWrapper,
+  electron,
 }:
 let
   packageName = "filen-desktop";
   packageVersion = "3.0.47";
   desktopName = "Filen Desktop";
+  appName = "Filen";
+
   desktopItem = makeDesktopItem {
     name = packageName;
     exec = packageName;
@@ -29,9 +34,6 @@ let
       "encrypted"
     ];
   };
-
-  iconPrefix = if stdenv.hostPlatform.isDarwin then "darwin" else "linux";
-  iconSuffix = if stdenv.hostPlatform.isDarwin then "icns" else "png";
 in
 buildNpmPackage {
   pname = packageName;
@@ -46,61 +48,102 @@ buildNpmPackage {
   };
 
   npmDepsHash = "sha256-+Ul2z6faZvAeCHq35janVTUNoqTQ5JNDeLbCV220nFU=";
-  npmBuildScript = "build";
 
-  # Prevent attempts to download electron binaries during build
-  ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
-  PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = "1";
-
-  # Dependencies only at time of build
   nativeBuildInputs = [
     pkgs.pkg-config
-    pkgs.electron
-    pkgs.makeWrapper
+    makeWrapper
   ]
-  ++ lib.optionals stdenv.isDarwin [
-    desktopToDarwinBundle
+  ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [
+    copyDesktopItems
+    imagemagick
   ];
 
-  # Dependencies also needed at runtime
   buildInputs = [
     pkgs.pixman
     pkgs.cairo
     pkgs.pango
   ];
 
-  # Override package-lock.json electron version to use what's given by nixpkgs
+  env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+  env.PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = "1";
+
   postPatch = ''
-    substituteInPlace package.json \
-      --replace-fail '"electron": "^34.1.1"' '"electron": "*"'
+        # Use nixpkgs electron instead of downloading
+        substituteInPlace package.json \
+          --replace-fail '"electron": "^34.1.1"' '"electron": "*"'
+        
+        # Fix app name and userData paths inside filen-desktop app source
+        substituteInPlace src/index.ts \
+          --replace-fail 'const options = await this.options.get()' \
+            'app.setName("${desktopName}")
+    		app.setPath("userData", pathModule.join(app.getPath("appData"), "@filen", "desktop"))
+    		
+    		const options = await this.options.get()'
+        
+        # Disable code signing (not needed for Nix)
+        substituteInPlace package.json \
+          --replace-fail '"afterSign": "build/notarize.js",' ""
   '';
 
-  # Set up icon assets in path required by desktopItem
-  preInstall = ''
-    mkdir -p $out/share/pixmaps
-    cp $src/assets/icons/app/${iconPrefix}.${iconSuffix} $out/share/pixmaps/${packageName}.${iconSuffix}
-    cp $src/assets/icons/app/${iconPrefix}Notification.${iconSuffix} $out/share/pixmaps/${packageName}-notification.${iconSuffix}
+  buildPhase = ''
+    runHook preBuild
+
+    # Build TypeScript
+    npm run build
+
+    # Build electron distributable bundle on darwin
+    ${lib.optionalString stdenv.hostPlatform.isDarwin ''
+      cp -r ${electron.dist} electron-dist
+      chmod -R u+w electron-dist
+
+      npx electron-builder \
+        --dir \
+        --mac \
+        -c.electronDist=electron-dist \
+        -c.electronVersion="${electron.version}"
+    ''}
+
+    runHook postBuild
   '';
 
-  # Create binary wrapper and desktopItem
-  # desktopItem auto-creates the .app bundle for Darwin
-  postInstall = ''
-    makeWrapper ${pkgs.electron}/bin/electron $out/bin/${packageName} \
-      --set-default ELECTRON_IS_DEV 0 \
-      --add-flags $out/lib/node_modules/@filen/desktop/dist/index.js \
-      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ]}"
+  installPhase = ''
+    runHook preInstall
 
-    mkdir -p $out/share/applications
-    cp ${desktopItem}/share/applications/* $out/share/applications/
+    ${
+      if stdenv.hostPlatform.isDarwin then
+        ''
+          # Install macOS .app bundle
+          mkdir -p $out/Applications
+          cp -r prod/mac*/${appName}.app $out/Applications/
+
+          # Create wrapper in bin
+          mkdir -p $out/bin
+          makeWrapper "$out/Applications/${appName}.app/Contents/MacOS/${appName}" $out/bin/${packageName}
+        ''
+      else
+        ''
+          # Install Linux version
+          mkdir -p $out/share/${packageName}
+          cp -r prod/*-unpacked/{locales,resources{,.pak}} $out/share/${packageName}
+
+          # Create desktop icons
+          mkdir -p $out/share/icons/hicolor/256x256/apps
+          magick assets/icons/app/linux.png -resize 256x256 $out/share/icons/hicolor/256x256/apps/${packageName}.png
+
+          # Create wrapper
+          makeWrapper ${lib.getExe electron} $out/bin/${packageName} \
+            --add-flags $out/share/${packageName}/resources/app.asar \
+            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
+            --inherit-argv0
+        ''
+    }
+
+    runHook postInstall
   '';
 
-  # Write correct darwin icons to .app contents
-  postFixup = lib.optionalString stdenv.hostPlatform.isDarwin ''
-    cp -rf $out/share/pixmaps/* "$out/Applications/${desktopName}.app/Contents/Resources"
-  '';
+  desktopItems = lib.optionals (!stdenv.hostPlatform.isDarwin) [ desktopItem ];
 
-  # Application Metadata
-  meta = with lib; {
+  meta = {
     homepage = "https://filen.io/products";
     downloadPage = "https://filen.io/products/desktop";
     description = "Filen Desktop Client";
@@ -109,9 +152,9 @@ buildNpmPackage {
       Sync your data, mount network drives, collaborate with others and access files natively powered by robust encryption and seamless integration.
     '';
     mainProgram = packageName;
-    platforms = platforms.linux ++ platforms.darwin;
-    license = licenses.agpl3Only;
-    maintainers = with maintainers; [
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+    license = lib.licenses.agpl3Only;
+    maintainers = with lib.maintainers; [
       smissingham
       kashw2
     ];
