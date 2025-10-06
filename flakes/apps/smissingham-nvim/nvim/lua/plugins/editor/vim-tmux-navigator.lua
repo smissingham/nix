9e490d844d5e426d74f6f000c3b34fa6c6caf6ef
Sean Missingham <sean@missingham.com>
2025-10-06 07:41:20 -0500

    TmuxNavigator Config Tweaks

--- flakes/apps/smissingham-nvim/nvim/lua/plugins/editor/vim-tmux-navigator.lua
+++ flakes/apps/smissingham-nvim/nvim/lua/plugins/editor/vim-tmux-navigator.lua
@@ -9,64 +9,21 @@ return {
 			"TmuxNavigatePrevious",
 			"TmuxNavigatorProcessList",
 		},
-		keys = {
-			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", mode = "n" },
-			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", mode = "n" },
-			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", mode = "n" },
-			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", mode = "n" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", mode = "n" },
-			{
-				"<c-h>",
-				function()
-					vim.cmd("TmuxNavigateLeft")
-				end,
-				mode = "t",
-			},
-			{
-				"<c-j>",
-				function()
-					vim.cmd("TmuxNavigateDown")
-				end,
-				mode = "t",
-			},
-			{
-				"<c-k>",
-				function()
-					vim.cmd("TmuxNavigateUp")
-				end,
-				mode = "t",
-			},
-			{
-				"<c-l>",
-				function()
-					vim.cmd("TmuxNavigateRight")
-				end,
-				mode = "t",
-			},
-			{
-				"<c-\\>",
-				function()
-					vim.cmd("TmuxNavigatePrevious")
-				end,
-				mode = "t",
-			},
-		},
 		init = function()
+			-- Reusable function to register keymaps in different contexts
+			local function set_keymaps()
+				vim.keymap.set({ "n", "t", "v" }, "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
+				vim.keymap.set({ "n", "t", "v" }, "<C-j>", "<cmd>TmuxNavigateDown<cr>")
+				vim.keymap.set({ "n", "t", "v" }, "<C-k>", "<cmd>TmuxNavigateUp<cr>")
+				vim.keymap.set({ "n", "t", "v" }, "<C-l>", "<cmd>TmuxNavigateRight<cr>")
+			end
+
+			-- Register once globally
+			set_keymaps()
+
+			-- Re-register for terminal buffers to prevent literal command injection
 			vim.api.nvim_create_autocmd("TermOpen", {
-				callback = function()
-					vim.keymap.set("t", "<C-h>", function()
-						vim.cmd("TmuxNavigateLeft")
-					end, { buffer = true })
-					vim.keymap.set("t", "<C-j>", function()
-						vim.cmd("TmuxNavigateDown")
-					end, { buffer = true })
-					vim.keymap.set("t", "<C-k>", function()
-						vim.cmd("TmuxNavigateUp")
-					end, { buffer = true })
-					vim.keymap.set("t", "<C-l>", function()
-						vim.cmd("TmuxNavigateRight")
-					end, { buffer = true })
-				end,
+				callback = set_keymaps,
 			})
 		end,
 	},
