6a5e280a057f407f60b297659e00c1bbd78b584a
Sean Missingham <sean@missingham.com>
2025-10-07 10:32:03 -0500

    Improve Markdown Obsidian Plugin Handling

--- flakes/apps/smissingham-nvim/nvim/lua/plugins/coding/markdown.lua
+++ flakes/apps/smissingham-nvim/nvim/lua/plugins/coding/markdown.lua
@@ -1,27 +1,29 @@
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		if vim.fn.getcwd():match("^" .. vim.fn.expand("~") .. "/Documents/Obsidian/") then
+			require("lazy").load({ plugins = { "obsidian.nvim" } })
+		end
+	end,
+})
+
 return {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		opts = {
-			file_types = { "markdown", "Avante" },
+			file_types = {
+				--"markdown",
+				"Avante",
+			},
 		},
-		ft = { "markdown", "Avante" },
 	},
 	{
 		"epwalsh/obsidian.nvim",
-		version = "*", -- recommended, use latest release instead of latest commit
-		lazy = true,
-		ft = "markdown",
-		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-		-- event = {
-		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-		--   -- refer to `:h file-pattern` for more examples
-		--   "BufReadPre path/to/my-vault/*.md",
-		--   "BufNewFile path/to/my-vault/*.md",
-		-- },
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
+		version = "*",
+		lazy = true,
+		event = {},
 		opts = {
 			workspaces = {
 				{
@@ -33,6 +35,17 @@ return {
 					path = "~/Documents/Obsidian/family/",
 				},
 			},
+			notes_subdir = "Slipbox",
+			new_notes_location = "notes_subdir",
+			note_id_func = function(title)
+				title = title or "Quick Note"
+				local prefix = os.date("%Y-%m-%d ")
+				return prefix .. title
+			end,
+		},
+		keys = {
+			{ "<leader>oo", "<cmd>ObsidianOpen<cr>", desc = "Obsidian Open" },
+			{ "<leader>on", "<cmd>ObsidianNew<cr>", desc = "Obsidian New" },
 		},
 	},
 }
