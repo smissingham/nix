086d28cb7af08b555dee4235753789a81b15284b
Sean Missingham <sean@missingham.com>
2025-11-02 22:12:44 -0600

    Wezterm Visual Touchups

--- flakes/apps/smissingham-nvim/nvim/lua/plugins/ui/themes.lua
+++ flakes/apps/smissingham-nvim/nvim/lua/plugins/ui/themes.lua
@@ -1,59 +1,59 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		opts = {
-			flavour = "mocha",
-			term_colors = true,
-			--transparent_background = true,
-		},
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function()
-			--			vim.cmd.colorscheme("tokyonight")
-		end,
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			compile = false,
-			undercurl = true,
-			commentStyle = { italic = true },
-			functionStyle = {},
-			keywordStyle = { italic = true },
-			statementStyle = { bold = true },
-			typeStyle = {},
-			transparent = false,
-			dimInactive = false,
-			terminalColors = true,
-			colors = {
-				palette = {},
-				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-			},
-			theme = "wave",
-			background = {
-				dark = "wave",
-				light = "lotus",
-			},
-		},
-	},
-	{
-		"navarasu/onedark.nvim",
-		priority = 1000,
-		config = function()
-			require("onedark").setup({
-				style = "deep",
-				transparent = true,
-			})
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      term_colors = true,
+      --transparent_background = true,
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      --			vim.cmd.colorscheme("tokyonight")
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      compile = false,
+      undercurl = true,
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false,
+      dimInactive = false,
+      terminalColors = true,
+      colors = {
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      theme = "wave",
+      background = {
+        dark = "wave",
+        light = "lotus",
+      },
+    },
+  },
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "deep",
+        --transparent = true,
+      })
 
-			vim.cmd.colorscheme("onedark")
-		end,
-	},
+      vim.cmd.colorscheme("onedark")
+    end,
+  },
 }
--- modules/shared/devtools/dots/terminals/wezterm/wezterm.lua
+++ modules/shared/devtools/dots/terminals/wezterm/wezterm.lua
@@ -1,21 +1,29 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+
+----- # General Display Settings # -----
 --config.color_scheme = "Catppuccin Mocha"
 --config.color_scheme = "Tokyo Night"
-config.color_scheme = "Catppuccin Macchiato"
-
 --config.font = wezterm.font("JetBrains Mono")
+config.max_fps = 120
+config.color_scheme = "Catppuccin Macchiato"
 config.font_size = 16
 
+----- # General Window Settings # -----
 config.window_background_opacity = 0.95
---config.window_decorations = "NONE" -- Doesnt play nice with aerospace :(
 config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+  left   = 5,
+  right  = 5,
+  top    = 5,
+  bottom = 5
+}
 
-config.max_fps = 120
+----- # Linux Related Settings # -----
 config.enable_wayland = false
 
---config.default_prog = { "/usr/bin/env", "nu" }
 
--- Finally, return the configuration to wezterm:
+
+
 return config
