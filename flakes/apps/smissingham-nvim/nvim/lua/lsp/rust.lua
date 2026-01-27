70da85162a20040a02f70a952d7b827a320f1718
Sean Missingham <sean@missingham.com>
2026-01-27 09:46:15 -0600

    Disable RustaceanVim

--- flakes/apps/smissingham-nvim/nvim/lua/lsp/rust.lua
+++ flakes/apps/smissingham-nvim/nvim/lua/lsp/rust.lua
@@ -1,15 +1,15 @@
 -- Rust LSP Server
--- vim.lsp.config("rust_analyzer", {
---   root_markers = {
---     "Cargo.toml"
---   },
---   settings = {
---     ["rust-analyzer"] = {
---       check = {
---         command = "clippy",
---       },
---     },
---   },
--- })
--- vim.lsp.enable("rust_analyzer")
+vim.lsp.config("rust_analyzer", {
+  root_markers = {
+    "Cargo.toml"
+  },
+  settings = {
+    ["rust-analyzer"] = {
+      check = {
+        command = "clippy",
+      },
+    },
+  },
+})
+vim.lsp.enable("rust_analyzer")
 -- Disabled, now configured by rustaceanvim plugin
--- flakes/apps/smissingham-nvim/nvim/lua/plugins/editor/debugging.lua
+++ flakes/apps/smissingham-nvim/nvim/lua/plugins/editor/debugging.lua
@@ -11,11 +11,11 @@ return {
         "theHamsta/nvim-dap-virtual-text",
         opts = {},
       },
-      {
-        'mrcjkb/rustaceanvim',
-        version = '^6', -- Recommended
-        lazy = false, -- This plugin is already lazy
-      }
+      -- {
+      --   'mrcjkb/rustaceanvim',
+      --   version = '^6', -- Recommended
+      --   lazy = false, -- This plugin is already lazy
+      -- }
     },
 
     -- stylua: ignore
